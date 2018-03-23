08ae4fadd6d9b74e155c02ebc14f21f90b659a6a
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2018-03-23 12:12:54 +0100

    toast added

--- src/pages/user-edit/user-edit.ts
+++ src/pages/user-edit/user-edit.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth';
 import { UserProvider } from '../../providers/user';
 
@@ -14,15 +14,37 @@ export class UserEditPage {
   password: String;
   userId: String;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public auth: AuthProvider,public userServ: UserProvider) {
-    console.log(this.auth)
-    this.user = this.auth.user;
-    this.username = this.user.username;
-    this.userId = this.user._id
-  }
+  constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams,
+      public auth: AuthProvider,
+      public userServ: UserProvider, 
+      public toast: ToastController) {
+        this.user = this.auth.user;
+        this.username = this.user.username;
+        this.userId = this.user._id
+    }
 
   updateUserDetails(){
-    this.userServ.updateUser(this.username, this.password, this.userId).subscribe()
+    this.userServ.updateUser(this.username, this.password, this.userId).subscribe(()=>{
+      this.presentToast()
+    })
+  }
+
+  presentToast() {
+    let toast = this.toast.create({
+      message: 'El usuario se ha editado correctamente',
+      duration: 3000,
+      position: 'middle',
+      showCloseButton: true,
+      closeButtonText: 'X'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
   }
 
 }
